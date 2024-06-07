f009d7c932b6d32a35d77c42329f1a4b71c499a5
Gustavo Inacio <gutiinacio@gmai.com>
2024-06-07 06:47:10 -0300

    feat: adding testemonials section

--- public/assets/javascripts/landingPage.js
+++ public/assets/javascripts/landingPage.js
@@ -9,4 +9,13 @@ window.onload = (event) => {
             prevEl: ".lp-control-swiper-categorie-button-prev",
           },
     });
+    var swiper2 = new Swiper(".lp-testemonial-swiper", {
+        slidesPerView: "auto",
+        spaceBetween: 30,
+        loop: true,
+        navigation: {
+            nextEl: ".lp-control-swiper-testemonial-button-next",
+            prevEl: ".lp-control-swiper-testemonial-button-prev",
+          },
+    });
 };
