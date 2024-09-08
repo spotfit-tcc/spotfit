db6098d92df4495b1c7ed0dbb45e66f4fcec9c19
Ana Yukimoto <anaelizayuki@gmail.com>
2024-09-08 16:47:20 -0300

    planview controller

---
+++ app/controllers/PlanViewController.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\controllers;
+
+use App\controllers\ApplicationController;
+
+class PlanViewController extends ApplicationController{
+    public function index(){
+        $this->render('index');
+    }
+}
+
+?>
