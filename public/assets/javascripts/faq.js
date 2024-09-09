d7f89514a4626c4681d6a1f489f71d09b91c3e2b
Ana Yukimoto <anaelizayuki@gmail.com>
2024-09-08 23:57:56 -0300

    sending email to FAQ and improvements to swiper categories/testemoial

--- public/assets/javascripts/faq.js
+++ public/assets/javascripts/faq.js
@@ -1,7 +1,29 @@
-$(document).ready(function(){
-    $('.fa-plus').click(function(){
+$(document).ready(function() {
+  // Toggle do conteúdo do card
+  $('.fa-plus').click(function() {
       var $cardBody = $(this).closest('.card').find('.card-body');
       $cardBody.toggle();
       $(this).toggleClass('fa-plus fa-minus');
-    });
   });
+
+  // Inicializa o EmailJS
+  emailjs.init("tcIQ-MEIxI6wOTrBg");
+
+  // Configura o envio do formulário
+  $('#contact-form').on('submit', function(event) {
+      event.preventDefault();
+      emailjs.sendForm('service_aeu1pvh', 'template_hb4ic34', this)
+          .then(() => {
+              console.log('SUCCESS!');
+              return Swal.fire('Boa!', 'E-mail enviado com sucesso!', 'success');
+          })
+          .then(() => {
+              // Limpa o formulário após a mensagem de sucesso ser fechada
+              $('#contact-form')[0].reset();
+          })
+          .catch((error) => {
+              console.log('FAILED...', error);
+              Swal.fire('Ops...', 'Algo deu errado, tente novamente!', 'error');
+          });
+  });
+});
--- public/assets/javascripts/landingPage.js
+++ public/assets/javascripts/landingPage.js
@@ -1,21 +1,52 @@
-window.onload = (event) => {
-    var swiper = new Swiper(".lp-categories-swiper", {
-        slidesPerView: 5,
-        spaceBetween: 30,
-        loop: true,
 
-        navigation: {
-            nextEl: ".lp-control-swiper-categorie-button-next",
-            prevEl: ".lp-control-swiper-categorie-button-prev",
-          },
-    });
-    var swiper2 = new Swiper(".lp-testemonial-swiper", {
-        slidesPerView: "auto",
-        spaceBetween: 30,
-        loop: true,
-        navigation: {
-            nextEl: ".lp-control-swiper-testemonial-button-next",
-            prevEl: ".lp-control-swiper-testemonial-button-prev",
-          },
-    });
-};
+var swiperCategories = new Swiper('.lp-categories-swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    autoplay: {
+        delay: 3000,
+        disableOnInteraction: false,
+    },
+    speed: 2000,
+    navigation: {
+        nextEl: '.lp-control-swiper-categorie-button-next',
+        prevEl: '.lp-control-swiper-categorie-button-prev',
+    },
+    loop: true,
+    breakpoints: {
+        1024: {
+            slidesPerView: 4,
+        },
+        768: {
+            slidesPerView: 3,
+        },
+        480: {
+            slidesPerView: 2,
+        },
+    },
+});
+
+var swiperTestemonials = new Swiper('.lp-testemonial-swiper', {
+    slidesPerView: 'auto',
+    spaceBetween: 20,
+    autoplay: {
+        delay: 5000,
+        disableOnInteraction: false,
+    },
+    speed: 5000,
+    navigation: {
+        nextEl: '.lp-control-swiper-testemonial-button-next',
+        prevEl: '.lp-control-swiper-testemonial-button-prev',
+    },
+    loop: true,
+    breakpoints: {
+        1024: {
+            slidesPerView: 2,
+        },
+        768: {
+            slidesPerView: 1,
+        },
+        480: {
+            slidesPerView: 1,
+        },
+    },
+});
