f4067e9b2645f09ca2384a910e1f38ef0a181015
Ana Yukimoto <anaelizayuki@gmail.com>
2024-10-30 22:08:36 -0300

    show profile data

--- app/controllers/ProfileController.php
+++ app/controllers/ProfileController.php
@@ -3,29 +3,31 @@
 namespace App\controllers;
 
 use App\controllers\ApplicationController;
-use App\models\UserLogged;
+use App\models\User;
+use App\models\BaseModel;
 
 class ProfileController extends ApplicationController {
     public function index(){
         // Se necessário, você pode redirecionar ou renderizar uma view padrão aqui
         $this->render('index');
-    }
-    public function account() {
-        if (isset($_GET['user_id']) && $_GET['user_id'] == $_SESSION['user_id']) {
-            $userLogged = new UserLogged();
-            $userData = $userLogged->getUserById($_SESSION['user_id']);
-            
-            // Verifique se o $userData não está vazio antes de renderizar
-            if ($userData) {
-                $this->render('profile/index', ['userData' => $userData]);
-            } else {
-                echo "Usuário não encontrado.";
-            }
+
+    $userData = User::logged_user(false);
+
+        // Verifica se o usuário está logado
+        if ($userData) {
+            // Renderiza a view com os dados do usuário
+            $this->render('profile', [
+                'profile_photo' => $userData['profile_photo'],
+                'user_name' => $userData['user_name'],
+                'email' => $userData['email'],
+                'phone' => $userData['phone'],
+            ]);
         } else {
-            echo "Acesso negado ou usuário não encontrado.";
+            // Redireciona para a página de login caso o usuário não esteja logado
+            $this->redirect('sign_in'); 
         }
-    }
     
+}
 }
 
 
