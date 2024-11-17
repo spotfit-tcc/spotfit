2571c2bb2d7e8e40e80f756f1351272537c526ee
Gustavo Inacio <gutiinacio@gmai.com>
2024-11-17 16:02:22 -0300

    avaliação na lp

--- app/controllers/LandingPageController.php
+++ app/controllers/LandingPageController.php
@@ -1,11 +1,17 @@
 <?php
 
 namespace App\controllers;
+use App\models\Comment;
 
 use App\controllers\ApplicationController;
 
 class LandingPageController extends ApplicationController{
     public function index(){
+        $comment_model = new Comment();
+        $comentarios = $comment_model->list_records();
+
+        $this->view->comentarios = $comentarios;
+
         $this->render('index');
     }
 }
